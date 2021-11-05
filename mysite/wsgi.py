afa82c37c275a5371960c28f2dda14122df42dba
munakaghamelu <mgodwinaghamelu@yahoo.co.uk>
2021-11-05 14:45:25 +0000

    can't find settings

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
+#activate_this = '/home/muna/Documents/django-experiments/django-env/bin/activate'
+#execfile(activate_this, dict(__file__=activate_this))
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-experiments.settings')
 
 application = get_wsgi_application()
